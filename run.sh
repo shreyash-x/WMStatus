c6f0acc6c2ce84241fd63e6093fafd169c4e8ed4
matta-varun <varunkaleb@gmail.com>
2023-07-26 08:31:55 +0000

    Fixed merge conflict in run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-<<<<<<< HEAD
 # Get the current date and time in the desired format
 current_date_time=$(date '+%Y-%m-%d %H:%M:%S')
 
@@ -10,7 +9,5 @@ export NVM_DIR=/home/kg766/.nvm
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm use 16.16.0
 
-=======
->>>>>>> a83396f2ef99ee4ed44e0069e086d36f8db69f09
 cd /home/kg766/whatsappMonitor/downloadTool
 npm run main
